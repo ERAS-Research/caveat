76a69a5b158db5a2426731acfd30444a317d4340
unbtorsten <torsten.reuschel@unb.ca>
2025-05-04 17:26:08 -0300

    link to build binary

--- demo_hack/run_direct.sh
+++ demo_hack/run_direct.sh
@@ -1,3 +1,3 @@
 #call directly without recompile, cf. https://docs.cocotb.org/en/stable/custom_flows.html
 #$(shell cocotb-config --makefiles)/Makefile.sim
-MODULE=cocotb_tb vvp -M $(cocotb-config --lib-dir) -m $(cocotb-config --lib-name vpi icarus) trxpc1_core
+MODULE=cocotb_tb vvp -M $(cocotb-config --lib-dir) -m $(cocotb-config --lib-name vpi icarus) build/trxpc1_core
