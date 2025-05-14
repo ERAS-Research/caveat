64ab6ee5757b86bafb40e79fb0bf36a1d3cbd436
unbtorsten <torsten.reuschel@unb.ca>
2025-05-14 19:45:13 -0300

    create stub for static tests and deisgn/xdc specification import

---
+++ src/caveat/xdc_parser.py
@@ -0,0 +1,10 @@
+# Copyright (C) 2025 ERAS Research Group and sanimut Development Team
+# Author(s): Torsten Reuschel
+
+"""
+Parse xdc constraint files
+"""
+
+def read_xdc(infilenames: list):
+    for infilename in infilenames:
+        print(infilename)
