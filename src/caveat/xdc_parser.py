0d7eed5aa531c0bf61e8add9c8ed33fdffd0f0a3
unbtorsten <torsten.reuschel@unb.ca>
2025-05-15 14:40:01 -0300

    add IOPin class, add comment to function

--- src/caveat/xdc_parser.py
+++ src/caveat/xdc_parser.py
@@ -5,7 +5,10 @@
 Parse xdc constraint files
 """
 
+
 def read_xdc(infilenames: list):
+    """Read in xdc constraint files
+    """
     for infilename in infilenames:
         print(infilename)
     return len(infilenames)
