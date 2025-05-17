f6ad287671903bb4af3b28ceec6ebd04d7e99a68
unbtorsten <torsten.reuschel@unb.ca>
2025-05-16 21:01:46 -0300

    contribute to #16

---
+++ src/caveat/tests/static/test_output_pin_current_drive_strength.py
@@ -0,0 +1,73 @@
+# Copyright (C) 2025 ERAS Research Group and sanimut Development Team
+# Author(s): Torsten Reuschel
+
+"""
+sanimut static verification
+"""
+
+
+import math
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))  #FIXME: refactor module import
+from caveat import xilinx_package_spec
+
+
+def test_output_pin_current_drive_strength(kintex7_iopin_specification,
+        net_specification, pin_constraints):
+    """Verify current driving strength with regard to package specification,
+    constraints, and netlist specification, i.e. schematic:
+        output pin Imax < Vbank/Rload?
+
+    Potential violations are highlighted using warnings, since the maximum
+    current refers to driving strength of a pin. This test does not check
+    whether a pin is used as output.
+    On the other hand, if the netlist specification indicates a large load
+    resistance such as Rload=1MOhm for input pins, this test is practically
+    guaranteed to pass for all input pin configurations, because of the implied
+    current limit.
+    """
+    #verify presence of meaningful input data
+    assert kintex7_iopin_specification != None, "Missing Kintex7 vendor I/O pin specification"
+    assert net_specification != None, "Missing netlist design specification"
+    assert pin_constraints != None, "Missing netlist design specification"
+    assert pin_constraints != None, "Missing pin constraints"
+
+    for pin in pin_constraints:
+        pin_iotype = xilinx_package_spec.get_iotype(
+                        kintex7_iopin_specification,
+                        pin['name'])
+
+        iostandard_voltage = xilinx_package_spec.get_pin_voltage_max_from_iostandard(pin['IOSTANDARD'])
+        assert not math.isnan(iostandard_voltage), \
+            "Unrecognized iostandard: {:s} for pin {:s}".format(
+                pin['IOSTANDARD'],
+                pin['name'])
+
+        pin_current = xilinx_package_spec.get_pin_current_max_7series(
+                        pin['IOSTANDARD'],
+                        pin_iotype)
+        assert not math.isnan(pin_current), \
+            "Package specification is missing current drive rating for pin {:s}".format(
+                pin['name'])
+
+        net_voltage = net_specification.get(pin['name'], {}).get('voltage_max')
+        assert not math.isnan(net_voltage), \
+            "Netlist specification is missing voltage rating for pin {:s}".format(
+                pin['name'])
+
+        net_Rload = net_specification.get(pin['name'], {}).get('Rload')
+        assert not math.isnan(net_Rload), \
+            "Netlist specification is missing termination resistance for pin {:s}".format(
+                pin['name'])
+
+        if net_specification.get(pin['name'], {}).get('inout') in ['output', 'inout']:
+            assert iostandard_voltage/net_Rload < pin_current/1e3, \
+                "Insufficient driving strength for termination on pin {:s} for chosen I/O standard {:s}".format(
+                    pin['name'],
+                    pin['IOSTANDARD'])
+            assert net_voltage/net_Rload < pin_current/1e3, \
+                "Insufficient driving strength for termination on pin {:s} for specified netlist voltage {:.1f}V".format(
+                    pin['name'],
+                    net_voltage)
--- src/caveat/xilinx_package_spec.py
+++ src/caveat/xilinx_package_spec.py
@@ -37,10 +37,21 @@ def get_voltage_bank(pinbank_db, iopin: str):
     Import and parse Xilinx package descriptions as provided at
     https://www.xilinx.com/support/package-pinout-files.html
     """
-    assert iopin in pinbank_db.get(part).keys(), \
+    assert iopin in pinbank_db.keys(), \
         "Unknown pin: {:s}".format(iopin)
 
-    return pinbank_db.get(iopin).get('bank')
+    return pinbank_db.get(iopin).get('Bank')
+
+
+def get_iotype(pinbank_db, iopin: str):
+    """
+    Import and parse Xilinx package descriptions as provided at
+    https://www.xilinx.com/support/package-pinout-files.html
+    """
+    assert iopin in pinbank_db.keys(), \
+        "Unknown pin: {:s}".format(iopin)
+
+    return pinbank_db.get(iopin).get('I/OType')
 
 
 def get_pin_voltage_max_from_iostandard(iostandard: str):
@@ -71,16 +82,18 @@ def get_io_voltage_max_from_iotype_7series(iotype: str):
     return iotype_db.get(iotype, math.nan)
 
 
-def get_pin_current_max_from_iostandard_7series(iostandard: str, bank: str):
+def get_pin_current_max_7series(iostandard: str, iotype: str):
     """Information according to 7 Series FPGAs SelectIO Resources User Guide
     (UG471), Table 1-8
     """
     iostandard_db = {
+        'LVDS':     {'HR':24, 'HP':16}, #FIXME: unconfirmed information, cf. ug471
+        'LVDS_25':  {'HR':16, 'HP':math.nan}, #FIXME: unconfirmed information, cf. ug471
         'LVCMOS12': {'HR':12, 'HP':8},
         'LVCMOS15': {'HR':16, 'HP':16},
         'LVCMOS18': {'HR':24, 'HP':16},
-        'LVCMOS25': {'HR':16, 'HP':mat.nan},
-        'LVCMOS33': {'HR':16, 'HP':mat.nan},
-        'LVTTL': {'HR':24, 'HP':mat.nan},
+        'LVCMOS25': {'HR':16, 'HP':math.nan},
+        'LVCMOS33': {'HR':16, 'HP':math.nan},
+        'LVTTL': {'HR':24, 'HP':math.nan},
         }
-    return iostandard_db.get(iostandard, math.nan)
+    return iostandard_db.get(iostandard).get(iotype)
