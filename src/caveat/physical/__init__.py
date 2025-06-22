5871e1b9f61d9a01346d55502495d1cc940c91ca
unbtorsten <torsten.reuschel@unb.ca>
2025-06-22 19:17:56 -0300

    ease of access for physical simulation definitions

---
+++ src/caveat/physical/__init__.py
@@ -0,0 +1,6 @@
+#ease of access to module definitions
+from .simulation import PhysicalSimulation
+from .adder import Adder
+from .attenuator import Attenuator
+from .delayline import DelayLine
+from .loopback import Loopback
