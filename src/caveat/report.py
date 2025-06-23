faa9fd0007540a35a3788c3f7792c3d63240d7f4
unbtorsten <torsten.reuschel@unb.ca>
2025-06-23 19:05:53 -0300

    make report title more generic

--- src/caveat/report.py
+++ src/caveat/report.py
@@ -17,10 +17,10 @@ html_template = """
     <!DOCTYPE html>
     <html>
     <head>
-        <title>CAVEAT Signal Capture</title>
+        <title>{report_title}</title>
     </head>
     <body>
-        <h1>CAVEAT Monitoring for active signals within the Device Under Test </h1>
+        <h1>{report_title}</h1>
 
         {plot_divs}
     </body>
@@ -91,12 +91,16 @@ def get_html_plot_data(testname, data_dict, axis_dict=None, truncate=False):
             plot_data += mpld3.fig_to_html(fig)
         return plot_data
 
-def make_report(testname: str, cfg_plot: dict, outfilepath: str='../results/dynamic/'):
+def make_report(testname: str, cfg_plot: dict,
+        report_title: str='CAVEAT signal capture',
+        outfilepath: str='../results/dynamic/'):
     """Compile stringified matplotlib figures into HTML report
     """
     #create report
     plot_data = get_html_plot_data(testname, **cfg_plot)
-    html_report = html_template.format(plot_divs = plot_data)
+    html_report = html_template.format(
+                    report_title=report_title,
+                    plot_divs=plot_data)
 
     #write report to file
     outfilesubdir = os.path.split(testname)[0]
