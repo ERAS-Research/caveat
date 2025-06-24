2e867c50988fcea694d83510e12a646f7b95dea4
mferris <mferris3@unb.ca>
2025-06-23 21:01:37 -0300

    working axis monitor

--- src/caveat/report.py
+++ src/caveat/report.py
@@ -40,43 +40,138 @@ def get_html_plot_data(testname, data_dict, axis_dict=None, truncate=False, head
         plot_data+="<br>"
         plot_data+="<br>"
         wavedrom_dict={"signal":[] }
-
+        datastrings={}
+        labels={}
+        counters={}
+        ##sort throguh all signals
+        uptimes=[]
+        downtimes=[]
         for signal in axis_dict:
-
-            data_list = axis_dict[signal]
-            previous = 0
-
-            datastring=""
-            datalist=[]
+            datastrings[signal]=""
+            labels[signal]=[]
+            data_list=axis_dict[signal]
+            counters[signal]=0
             for datum in data_list:
                 data, start, end, period = datum
+                uptimes.append([int(start),int(end), signal, data, int(period)])
+        ## now to sort, shouldnt't matter which i sort by for overlap but I'll sort by start time to ensure that 0 time entries are first
 
-                header = data[:header_size]
-                if (start-previous>0):
-                    datastring+="=...."
-                    label="idle: {} ns".format(start-previous)
-                    datalist.append(label)
-                ##this will add a length of 5 to your data
 
-                datastring_builder="="
+        uptimes.sort(key= lambda x: x[0])
+
+        for xx in range(len(uptimes)-1):
+            if uptimes[xx+1][0]>(uptimes[xx][1]+4*uptimes[xx][4]):
+                downtimes.append([uptimes[xx][1], uptimes[xx+1][0], [signal for signal in axis_dict], 0, uptimes[xx][4]])##downtimes apply to all charts at once
+
+        print("downtimes:", downtimes)
+        relevant_times=downtimes+uptimes
+
+        relevant_times.sort(key= lambda x: x[0])
+        print("relevant times are",relevant_times)
+        for entry in relevant_times:
+            print("entry is ", entry)
+            signal=entry[2]
+            if entry[3]==0:##downtimes
+                print("EXECUTING DOWNTIME -------------------------------------------------------------- \n \n \n")
+                for xx in axis_dict:
+                    if counters[xx]<entry[0]:
+                        print("counter", xx, "is", counters[xx], "current count at", entry[0])
+                        diff=int((entry[0]-counters[xx])/entry[4])
+                        print("diff is", diff)
+                        datastring='x'
+                        for __ in range(diff-1):
+                            datastring+='.'
+                        datastrings[xx]+=datastring
+                        counters[xx]=entry[0]
+
+                    datastrings[xx]+="=..."
+                    labels[xx].append("idle: {} ns".format(entry[1]-entry[0]))
+                    counters[xx]=entry[1]
+            else: ##uptimes:
+
+                if counters[signal]<entry[0]:
+                    diff=int((entry[0]-counters[signal])/entry[4])
+                    datastring='x'
+                    for __ in range(diff-1):
+                        datastring+='.'
+                    datastrings[signal]+=datastring
+                    counters[signal]=entry[0]
+
+
+                datastring="="
                 for __ in range(header_size-1):
-                    datastring_builder+="."##FIXME: manual appending of header length
-                label="H"
-                datalist.append(label)
-                datastring+=datastring_builder
+                    datastring+="."
 
+                datastrings[signal]+=datastring
+                labels[signal].append("H")
 
-                if len(data) > header_size:
+                if len(entry[3]) > header_size:
                     datastring_builder="="
-                    print("data length is ",len(data))
-                    for __ in range(len(data)-(header_size+1)):
+                    print("data length is ",len(entry[3]))
+                    for __ in range(len(entry[3])-(header_size+1)):
                         datastring_builder += "."
-                    print("data is", data)
-                    datastring+=datastring_builder
+                    print("data is", entry[3])
+                    datastrings[signal]+=datastring_builder
                     label="P"
-                    datalist.append(label)
-                previous = end
-            wavedrom_dict["signal"].append({"name":signal, "wave": datastring, "data": datalist})
+                    labels[signal].append(label)
+                counters[signal]=entry[1]
+                print("hit end of uptime, counter at", counters[signal])
+        for signal in axis_dict:
+            wavedrom_dict["signal"].append({"name":signal, "wave": datastrings[signal], "data": labels[signal]})
+
+
+
+
+
+
+
+
+
+
+
+        # for signal in axis_dict:
+
+        #     data_list = axis_dict[signal]
+        #     previous = 0
+        #     downtime_ptr=0
+        #     datastring=""
+        #     datalist=[]
+
+
+
+        #     for datum in data_list:
+        #         data, start, end, period = datum
+        #         if (previous<downtimes[downtime_ptr][0] and start>downtimes[downtime_ptr][1]):
+        #             datastring+="=.|.."
+
+        #             label="idle: {} ns".format(start-previous)
+        #             datalist.append(label)
+        #         header = data[:header_size]
+        #         if (start-previous>0):
+        #             datastring+="=...."
+        #             label="idle: {} ns".format(start-previous)
+        #             datalist.append(label)
+        #         ##this will add a length of 5 to your data
+
+        #         datastring_builder="="
+        #         for __ in range(header_size-1):
+        #             datastring_builder+="."
+        #         label="H"
+        #         datalist.append(label)
+        #         datastring+=datastring_builder
+
+
+        #         if len(data) > header_size:
+        #             datastring_builder="="
+        #             print("data length is ",len(data))
+        #             for __ in range(len(data)-(header_size+1)):
+        #                 datastring_builder += "."
+        #             print("data is", data)
+        #             datastring+=datastring_builder
+        #             label="P"
+        #             datalist.append(label)
+        #         previous = end
+
 
 
 
