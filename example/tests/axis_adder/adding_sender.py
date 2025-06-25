a5a2bc66eeda60e5f19b5759c37383789183e9f3
mferris <mferris3@unb.ca>
2025-06-25 16:46:41 -0300

    cleanup

--- example/tests/axis_adder/adding_sender.py
+++ example/tests/axis_adder/adding_sender.py
@@ -26,37 +26,6 @@ from caveat import physical
 
 
 
-# def communication_operation(caveat_socket):
-#     """Main thread to pass packets between interfaces
-#     """
-#     #set timeout for blocking loop in case of no incoming packets
-#     caveat_socket.settimeout(0)
-#     stop=False
-#     buffer_size = 8192
-#     while not stop:
-#         #forward packets from socket to AXIS
-#         message = None
-#         try:
-#             message = caveat_socket.recv(buffer_size)
-#             print('SOCK>DEV', list(message), flush=True)
-#         except:
-#             pass
-
-#         #foward packets from AXIS to socket
-#         try:
-#             message = self.axis_sink.recv_nowait(compact=True)
-#             if message:
-#                 if self.verbose:
-#                     print('DEV>SOCK', list(message), flush=True)
-#                 self.socket.sendto(bytearray(message), (self.remote_address, self.remote_port))
-#         except cocotb.queue.QueueEmpty:
-#             pass
-
-
-
-
-
-
 remote_address = "127.0.0.1"
 remote_port = 20002
 local_port = 20000
@@ -69,35 +38,31 @@ stop=False
 buffer_size = 8192
 
 
-
-
-
-
-
-
-#TODO: register DUT inputs for monitoring (to plot, etc...)
-
+print("adder over socket demonstration. print two numbers to add in the socket_adder test. send 100 100 to shut down both programs")
 #start emulator
 while True:
-    messageinput=None
-    message=input("numbers to add:")
-    message=message.strip().split()
-    received=None
-    if len(message)==2:
+    messageinput = None
+    message = input("numbers to add:")
+    message = message.strip().split()
+    received = None
+    if len(message )== 2:
         try:
             message = [int(xx) for xx in message]
             caveat_socket.sendto(bytearray(message), (remote_address, remote_port))
+            if message == [100,100]:
+                print("shutdown engaged")
+                break
             print("waiting for response...")
             start=time.time()
             while True:
-                current=time.time()
-                if current-start>5:
-                    print("timeout")
-                    received=None
+                current = time.time()
+                if current - start>1:
+                    print("timeout. Numbers may be beyond selected bitwidth (default is (2^4)-1 = 15)")
+                    received = None
                     break
                 try:
                     message = caveat_socket.recv(buffer_size)
-                    print('SOCK>DEV', list(message), flush=True)
+                    print('Received sum:', list(message)[0], flush=True)
                     break
                 except:
                     pass
@@ -110,8 +75,5 @@ while True:
 
 
 #clean up
-#FIXME: never reaching this, if receiving SIGINT or SIGTERM; this should be triggered through an independent process
-dut._if_socket_handle.stop = True
-dut._if_socket_handle.communication_stop()
 
 
