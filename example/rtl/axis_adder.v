8351d3cabd5183e4278052a7e55bbc70b4bc3fe8
unbtorsten <torsten.reuschel@unb.ca>
2025-06-26 21:36:03 -0300

    some more refactoring

--- example/rtl/axis_adder.v
+++ example/rtl/axis_adder.v
@@ -1,16 +1,16 @@
 `default_nettype none
 
 module axis_adder #(
-  parameter DATA_WIDTH = 8
+  parameter c_WIDTH = 8
 ) (
   input wire clk,
   input wire rst,
 
-  input  wire [2*DATA_WIDTH-1:0] s_axis_tdata,
+  input  wire [2*c_WIDTH-1:0] s_axis_tdata,
   input  wire                    s_axis_tvalid,
   output reg                     s_axis_tready= 1,
 
-  output reg [DATA_WIDTH-1:0]  m_axis_tdata,
+  output reg [c_WIDTH-1:0]  m_axis_tdata,
   output reg                   m_axis_tvalid = 0,
   input  wire                  m_axis_tready
 );
@@ -22,7 +22,7 @@ always @(posedge clk) begin
   end
 
   if (s_axis_tvalid && s_axis_tready) begin
-    m_axis_tdata <= s_axis_tdata[(2*DATA_WIDTH-1):DATA_WIDTH]+s_axis_tdata[DATA_WIDTH-1:0];
+    m_axis_tdata <= s_axis_tdata[2*c_WIDTH-1:c_WIDTH]+s_axis_tdata[c_WIDTH-1:0];
     m_axis_tvalid <= 1;
     s_axis_tready <= 0;
   end
