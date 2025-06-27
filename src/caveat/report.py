e39e83d94f52a8ae6ffb6365a17fb40eda5215bd
unbtorsten <torsten.reuschel@unb.ca>
2025-06-27 11:31:13 -0300

    reformatting

--- src/caveat/report.py
+++ src/caveat/report.py
@@ -137,10 +137,10 @@ def get_html_plot_data(testname, data_dict, axis_dict=None, truncate=False, head
             dx = list(dx)
             for ii, xx in enumerate(dx):
                 if ('z' in str(xx)) or ('x' in str(xx)):
-                    dx[ii] = [float('nan')]*len(dx[ii])
+                    dx[ii] = [float('nan')] * len(dx[ii])
                 else:
                     if isinstance(dx[ii], str):
-                        dx[ii]=float(xx)
+                        dx[ii] = float(xx)
                     else:
                         dx[ii] = [float(xx) for xx in dx[ii]]
             #plot data
