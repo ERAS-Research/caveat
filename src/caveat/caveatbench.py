29fcf769575110cc957af979b514a5c4d92118b7
mferris <mferris3@unb.ca>
2025-07-04 13:24:26 -0300

    incorporate feedback and abstract plotting

--- src/caveat/caveatbench.py
+++ src/caveat/caveatbench.py
@@ -75,7 +75,7 @@ class CaveatBench():
             bus = AxiStreamBus.from_prefix(self.dut, prefix)
             self.sources[label] = AxiStreamSource(
                 AxiStreamBus.from_prefix(self.dut, prefix),
-                clk, byte_size=byte_width)
+                clk, byte_size=data_bitwidth)
             #configure logging
             logging.getLogger('cocotb.{:s}.{:s}'.format(str(self.dut), prefix)).setLevel(verbosity_level)
         else:
@@ -84,12 +84,12 @@ class CaveatBench():
                 bus._add_signal(key, prefix + value)
                 #configure logging
                 logging.getLogger('cocotb.{:s}.{:s}'.format(str(self.dut), prefix + value)).setLevel(verbosity_level)
-            self.sources[label] = AxiStreamSource(bus, clk, byte_size=byte_width)
+            self.sources[label] = AxiStreamSource(bus, clk, byte_size=data_bitwidth)
         if monitor:
             self.monitors[label] = CaveatAxiStreamMonitor(bus, clk)
 
     async def add_receiver_axis(self, label: str, clk, prefix: str='',
-            signals: dict={}, verbosity_level=logging.WARNING, byte_width=8, monitor=False):
+            signals: dict={}, verbosity_level=logging.WARNING, data_bitwidth=8, monitor=False):
         """Add AXI-Stream interface capable of receiving data from the DUT.
         Takes in either a shared prefix for the prefix_t* wires in the HDL code,
         or a dictionary of signals following the pattern
@@ -107,7 +107,7 @@ class CaveatBench():
             bus = AxiStreamBus.from_prefix(self.dut, prefix)
             self.sinks[label] = AxiStreamSink(
                                     AxiStreamBus.from_prefix(self.dut, prefix),
-                                    clk, byte_size=byte_width)
+                                    clk, byte_size=data_bitwidth)
 
             #configure logging
             logging.getLogger('cocotb.{:s}.{:s}'.format(str(self.dut), prefix)).setLevel(verbosity_level)
@@ -117,7 +117,7 @@ class CaveatBench():
                 bus._add_signal(key, prefix + value)
                 #configure logging
                 logging.getLogger('cocotb.{:s}.{:s}'.format(str(bus), prefix + value)).setLevel(verbosity_level)
-            self.sinks[label] = AxiStreamSink(bus, clk, byte_size=byte_width)
+            self.sinks[label] = AxiStreamSink(bus, clk, byte_size=data_bitwidth)
         if monitor:
             self.monitors[label] = CaveatAxiStreamMonitor(bus, clk)
 
