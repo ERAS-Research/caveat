359c7857616ede88068b9c3bb412682fc17002f5
unbtorsten <torsten.reuschel@unb.ca>
2025-07-17 16:40:11 -0300

    cleanup

--- example/tests/test_axis_loopback.py
+++ example/tests/test_axis_loopback.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # Copyright (C) 2025 ERAS Research Group and sanimut Development Team
 # Author(s): Murray Ferris, Torsten Reuschel
 
@@ -15,23 +17,28 @@ from caveat.caveatbench import CaveatBench
 
 @cocotb.test()
 async def loopback_throughput(dut):
-    """Pass a random sequence of bytes through a FIFO via AXI.
+    """Pass a random sequence of bytes through AXIS.
     """
     #create testbench
     tb = CaveatBench(dut)
+    #..initialize clock and input/output interfaces
     await tb.initialize_clock('clk', 10)
     await tb.add_sender_axis(prefix='s_axis', label='input', clk='clk')
     await tb.add_receiver_axis(prefix='m_axis', label='output', clk='clk')
+    #..set up monitoring
     for signal in ['s_axis_tdata', 'm_axis_tdata']:
         await tb.init_monitor(signal)
+
     #pass sequence
     input_sequence = [random.randint(0,255) for xx in range(100)]
-    await tb.wait("clk",1)
+    await tb.wait('clk', 1)
     await tb.send_message('input', input_sequence)
     received_sequence = await tb.read_message('output')
     assert received_sequence == input_sequence, 'Sequence not received as sent.'
+
     #export illustration of data transfer
     tb.generate_plot()
+
 
 def test_loopback_throughput():
     """pytest wrapper to capture test results from cocotb_test runner
