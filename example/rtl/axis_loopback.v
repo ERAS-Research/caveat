8162156a3aecabad5b4d4c53509d4a53ed6e9cf5
unbtorsten <torsten.reuschel@unb.ca>
2025-07-17 16:30:08 -0300

    clean up examples

--- example/rtl/axis_loopback.v
+++ example/rtl/axis_loopback.v
@@ -5,15 +5,15 @@ module axis_loopback #(
 ) (
   input wire clk,
 
-  input  wire [c_WIDTH-1:0]   s_axis_tdata,
-  input  wire                 s_axis_tvalid,
-  output wire                 s_axis_tready,
-  input wire                  s_axis_tlast,
+  input  wire [c_WIDTH-1:0] s_axis_tdata,
+  input  wire               s_axis_tvalid,
+  output wire               s_axis_tready,
+  input wire                s_axis_tlast,
 
-  output wire [c_WIDTH-1:0]   m_axis_tdata,
-  output wire                 m_axis_tvalid,
-  input  wire                 m_axis_tready,
-  output wire                 m_axis_tlast
+  output wire [c_WIDTH-1:0] m_axis_tdata,
+  output wire               m_axis_tvalid,
+  input  wire               m_axis_tready,
+  output wire               m_axis_tlast
 );
 
 assign m_axis_tdata = s_axis_tdata;
